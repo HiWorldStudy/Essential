bd0598aa6f49b31f97ef33e551cb3bc44b904e90
AhReum <hi.world.study@gmail.com>
2026-02-17 21:20:42 +0900

    ~ CH03_05

--- CH03_02/app.js
+++ CH03_02/app.js
@@ -1,19 +1,65 @@
 const ajax = new XMLHttpRequest();
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+const container = document.getElementById('root');
+const content = document.createElement(`div`);
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL =  'https://api.hnpwa.com/v0/item/@id.json';
 
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
 
 const ul = document.createElement('ul');
 
-for(let i = 0; i < 10; i++)
-{
-    const li = document.createElement('li');
-    li.innerHTML = `${newsFeed[i].title}`;
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    
+    const newsList = [];
+    newsList.push(`<ul>`);
 
-    ul.appendChild(li);
+    for(let i = 0; i < 10; i++)
+    {
+        newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+    newsList.push(`</ul>`);
+
+    container.innerHTML=newsList.join('');
 }
 
-document.getElementById('root').appendChild(ul);
+function newsDetail() {
+    const id = location.hash.substring(1);
 
+    const newsContent = getData(CONTENT_URL.replace(`@id`, id));
+
+    const title = document.createElement(`h1`);
+    container.innerHTML=`
+        <h1>${newsContent.title}</h1>
+
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+
+    if(routePath === ''){
+        newsFeed();
+    }
+    else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
