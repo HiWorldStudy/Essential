a20b5ea23148f9a9734560b521eddbe659e94569
AhReum <hi.world.study@gmail.com>
2026-02-17 21:49:18 +0900

    CH03_06 Paging

--- CH03_02/app.js
+++ CH03_02/app.js
@@ -3,6 +3,9 @@ const container = document.getElementById('root');
 const content = document.createElement(`div`);
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL =  'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open('GET', url, false);
@@ -15,15 +18,17 @@ const ul = document.createElement('ul');
 
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
-    
     const newsList = [];
+
+    const pageCount = newsFeed.length / 10;
+
     newsList.push(`<ul>`);
 
-    for(let i = 0; i < 10; i++)
+    for(let i = (store.currentPage - 1) * 10 ; i < (store.currentPage * 10); i++)
     {
         newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
@@ -31,11 +36,18 @@ function newsFeed() {
     }
     newsList.push(`</ul>`);
 
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage < pageCount ? store.currentPage + 1 : store.currentPage}">다음 페이지</a>
+        </div>
+    `);
+
     container.innerHTML=newsList.join('');
 }
 
 function newsDetail() {
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
 
     const newsContent = getData(CONTENT_URL.replace(`@id`, id));
 
@@ -44,7 +56,7 @@ function newsDetail() {
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -55,7 +67,11 @@ function router() {
     if(routePath === ''){
         newsFeed();
     }
-    else {
+    else if(routePath.indexOf('#/page/') >= 0){
+        store.currentPage = Number(routePath.substring(7));
+        newsFeed();
+    }
+    else{
         newsDetail();
     }
 }
